d96ddac26abd580460b0b1f88e68fdd3ce7e1d51
erwanpersonne <100788977+erwanpersonne@users.noreply.github.com>
2023-06-13 12:30:28 +0200

    Update initialization.c
    Include Text Licence V0

--- Code_source/initialization.c
+++ Code_source/initialization.c
@@ -1,3 +1,33 @@
+/*
+This file is part of SurfAtm software
+Copyright(c) 2023, â€“ UMR ECOSYS, AgroParisTech INRAe, France
+
+== GNU General Public License Usage ==
+
+SurfAtm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SurfAtm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SurfAtm. If not, see <http://www.gnu.org/licenses/>.
+
+== Other Usage ==
+Other Usage means a use of SurfAtm that is inconsistent with the GPL
+license, and requires a written agreement between You, AgroParisTech and INRAe.
+Licensees for Other Usage of SurfAtm may use this file in accordance
+with the terms contained in the written agreement between You, AgroParisTech and INRAe.
+*/
+/*
+@author Erwan Personne <erwan.personne@agroparistech.fr>
+@technical support : support.surfatm@agroparistech.fr
+*/
+
 /*----------Initialization of variable and parameters--------
 
 References:
@@ -41,12 +71,12 @@ void initialization(void)
 
 
 	// parameters
-	critmaxStab = 0.01;													// threshold for iteration convergence of the energy balance focused on T° - [in °C]  												
+	critmaxStab = 0.01;													// threshold for iteration convergence of the energy balance focused on TÂ° - [in Â°C]  												
 	incrmaxStab = 15;													// maximal number of itertion for stability temperature  
-	critmaxRn = 0.05;													// threshold for iteration convergence of the radiation budget focused on T° - [in °C]  												
+	critmaxRn = 0.05;													// threshold for iteration convergence of the radiation budget focused on TÂ° - [in Â°C]  												
 	incrmaxRn = 100;													// maximal number of itertion for radiation budget   
 
-	critmaxPprime = 0.01;												// threshold for iteration convergence of PPrime calculation focused on T° - [in °C]
+	critmaxPprime = 0.01;												// threshold for iteration convergence of PPrime calculation focused on TÂ° - [in Â°C]
 	incrmaxPprime = 10;													// maximal number of itertion for Pprime calculation 
 
 	Mulch_threshold = 0.5;												// ratio of the "mulch filling" before destruction during rain event - ranging between 0 and 1 (i.e., 0 and 100%)		 
