0513c3b0750752e45555a762e09a0900814a0ad5
erwanpersonne <100788977+erwanpersonne@users.noreply.github.com>
2023-06-13 12:26:22 +0200

    Update EnergyBalance.c
    Include text licence V0

--- Code_source/EnergyBalance.c
+++ Code_source/EnergyBalance.c
@@ -1,3 +1,33 @@
+/*
+This file is part of SurfAtm software
+Copyright(c) 2023, – UMR ECOSYS, AgroParisTech INRAe, France
+
+== GNU General Public License Usage ==
+
+SurfAtm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SurfAtm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SurfAtm. If not, see <http://www.gnu.org/licenses/>.
+
+== Other Usage ==
+Other Usage means a use of SurfAtm that is inconsistent with the GPL
+license, and requires a written agreement between You, AgroParisTech and INRAe.
+Licensees for Other Usage of SurfAtm may use this file in accordance
+with the terms contained in the written agreement between You, AgroParisTech and INRAe.
+*/
+/*
+@author Erwan Personne <erwan.personne@agroparistech.fr>
+@technical support : support.surfatm@agroparistech.fr
+*/
+
 /*----------Program for energy balance--------
 
 Here are the calculations of each terms of the energy balance (H, LE, and G) and their
@@ -12,7 +42,7 @@ balance and bi-directional exchanges of ammonia applied at the field scale.
 Biogeosciences,	6, 1371-1388.
 
 Choudhury, B.J., Monteith, J.L., 1988. A four-layer model for the heat budget of homogeneous land surfaces. 
-Quarterly Journal of the Royal Meteorological Society 114, 373�398. https://doi.org/10.1002/qj.49711448006
+Quarterly Journal of the Royal Meteorological Society 114, 373–398. https://doi.org/10.1002/qj.49711448006
 
 */
 
