4eb19b35b262e7a007a0a322307294d1a2722511
erwanpersonne <100788977+erwanpersonne@users.noreply.github.com>
2023-06-13 12:25:28 +0200

    Update Data_preparation.c
    Licence text V0

--- Code_source/Data_preparation.c
+++ Code_source/Data_preparation.c
@@ -1,10 +1,40 @@
+/*
+This file is part of SurfAtm software
+Copyright(c) 2023, – UMR ECOSYS, AgroParisTech INRAe, France
+
+== GNU General Public License Usage ==
+
+SurfAtm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SurfAtm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SurfAtm. If not, see <http://www.gnu.org/licenses/>.
+
+== Other Usage ==
+Other Usage means a use of SurfAtm that is inconsistent with the GPL
+license, and requires a written agreement between You, AgroParisTech and INRAe.
+Licensees for Other Usage of SurfAtm may use this file in accordance
+with the terms contained in the written agreement between You, AgroParisTech and INRAe.
+*/
+/*
+@author Erwan Personne <erwan.personne@agroparistech.fr>
+@technical support : support.surfatm@agroparistech.fr
+*/
+
 /*----------Program for preparation of the input data--------
 
 Some checks are done to avoid errors in calculation
 Calculation of vegetation and canopy albedos are also performed
 
 References:
-Montes, C., Lhomme, J.P., Demarty, J., Pr�vot, L., Jacob, F. (2014) A three-source
+Montes, C., Lhomme, J.P., Demarty, J., Prévot, L., Jacob, F. (2014) A three-source
 	SVAT modeling of evaporation: Application to the seasonal dynamics of a grassed 
 	vineyard. Agricultural and Forest Meteorology, 191, 64-80.
 
