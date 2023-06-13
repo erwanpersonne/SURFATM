cc3a453e9ae47352be58c44c78dbfdda83bc5d84
erwanpersonne <100788977+erwanpersonne@users.noreply.github.com>
2023-06-13 12:28:41 +0200

    Update SWC.evolution.c
    Include Text Licence V0

--- Code_source/SWC.evolution.c
+++ Code_source/SWC.evolution.c
@@ -1,3 +1,33 @@
+/*
+This file is part of SurfAtm software
+Copyright(c) 2023, â€“ UMR ECOSYS, AgroParisTech INRAe, France
+
+== GNU General Public License Usage ==
+
+SurfAtm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SurfAtm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SurfAtm. If not, see <http://www.gnu.org/licenses/>.
+
+== Other Usage ==
+Other Usage means a use of SurfAtm that is inconsistent with the GPL
+license, and requires a written agreement between You, AgroParisTech and INRAe.
+Licensees for Other Usage of SurfAtm may use this file in accordance
+with the terms contained in the written agreement between You, AgroParisTech and INRAe.
+*/
+/*
+@author Erwan Personne <erwan.personne@agroparistech.fr>
+@technical support : support.surfatm@agroparistech.fr
+*/
+
 /*----------Evolution of the Soil Water Content--------*/
 
 #include <stdio.h>
@@ -13,9 +43,9 @@ void SWC_evolution(void)
 {
 
 	double
-		evapmm	= 0.0		// Evaporation in mm                        [mm ou kg/m²]
-		, transpmm	= 0.0	// Transpiration in mm                      [mm ou kg/m²]
-		, Mulch_Max_SWC	= 0.0 //											[mm ou kg/m²]
+		evapmm	= 0.0		// Evaporation in mm                        [mm ou kg/mÂ²]
+		, transpmm	= 0.0	// Transpiration in mm                      [mm ou kg/mÂ²]
+		, Mulch_Max_SWC	= 0.0 //											[mm ou kg/mÂ²]
 		;
 
 // Conversion from [W/m2] to [kg/m2/timestep]
